59afc7250a61d0c65700720c5e80ab00b6c6bc22
yangzhudahushenqi <1163816848@qq.com>
2019-06-18 14:42:37 +0800

    增加了脚本

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -7,7 +7,18 @@ class TestLogin:
         assert 1
 
     def test_login3(self):
-        assert 0
+        assert 1
 
     def test_login4(self):
+        assert 0
+
+    def test_login5(self):
         assert 1
+
+
+    def test_login6(self):
+        assert 1
+
+
+    def test_login7(self):
+        assert 0
